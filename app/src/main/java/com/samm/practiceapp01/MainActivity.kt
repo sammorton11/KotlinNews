9237d25a8d9af9c102c04fd7d3082f8fe47f02ed
sammorton11 <sammorton11@yahoo.com>
2023-01-31 14:38:02 -0600

    Contact info fragment

--- app/src/main/java/com/samm/practiceapp01/MainActivity.kt
+++ app/src/main/java/com/samm/practiceapp01/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportActionBar?.setDisplayShowTitleEnabled(true)
-        supportActionBar?.title = "Kotlin Articles"
+        supportActionBar?.title = "Kotlin News"
     }
 }
--- app/src/main/java/com/samm/practiceapp01/presentation/ContactInfoFragment.kt
+++ app/src/main/java/com/samm/practiceapp01/presentation/ContactInfoFragment.kt
@@ -1,17 +1,37 @@
 package com.samm.practiceapp01.presentation
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import com.samm.practiceapp01.R
 
 class ContactInfoFragment : Fragment() {
+
+    private lateinit var clickableEmail: TextView
+    private lateinit var contactButton: Button
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        return inflater.inflate(R.layout.fragment_contact_info, container, false)
+        val view = inflater.inflate(R.layout.fragment_contact_info, container, false )
+        clickableEmail = view.findViewById(R.id.email)
+        contactButton = view.findViewById(R.id.contactButton)
+        contactButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SEND).apply {
+                type = "plain/text"
+                putExtra(Intent.EXTRA_EMAIL, arrayOf(clickableEmail.text))
+                putExtra(Intent.EXTRA_SUBJECT, "subject")
+                putExtra(Intent.EXTRA_TEXT, "mail body")
+            }
+            startActivity(Intent.createChooser(intent, "") )
+        }
+
+        return view
     }
 }
